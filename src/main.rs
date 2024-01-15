580d8afe48f55a126ae29c7418969d04b6b374c9
Kamil Chmielewski <chmielewski.kamil45@gmail.com>
2024-01-15 17:36:02 +0100

    👷 Return 200 OK on health_check

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+#[get("/health_check")]
+async fn health_check() -> impl Responder {
+    HttpResponse::Ok()
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(hello).service(health_check))
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
 }
